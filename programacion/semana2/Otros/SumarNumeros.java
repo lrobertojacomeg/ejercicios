13c2c13402c1d11f50bde7ea45fa1cb46ee50b9c
lrobertojacomeg <53920386+lrobertojacomeg@users.noreply.github.com>
2024-05-01 10:30:12 -0500

    Add files via upload

---
+++ programacion/semana2/Otros/SumarNumeros.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Otros;
+
+/**
+ *
+ * @author robbi
+ */
+public class SumarNumeros {
+
+  public static void main(String[] args) {
+    int num1 = 184843848;
+    int num2 = 345323393;
+    
+    int resultado = num1 + num2;
+    
+    System.out.println(resultado);
+  }
+
+}
